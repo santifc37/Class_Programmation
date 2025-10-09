b41cd68bfbd2a374c34c3cd77714f85820c89703
Santiago Giraldo <santifc089gmail.com>
2025-10-08 19:53:08 -0500

    Prueba

--- Class_Programmation/WeatherForecast.cs
+++ Class_Programmation/WeatherForecast.cs
@@ -9,5 +9,7 @@ namespace Class_Programmation
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
         public string? Summary { get; set; }
+
+        public string ValorPrueba { get; set; }
     }
 }
